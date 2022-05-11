e40e4d083c939965a61e4985ae16d50d1a98fe3d
igor-patrocinio <igoropatrocinio@gmail.com>
2022-05-10 22:59:45 -0300

    first commit

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -16,21 +16,3 @@ resource "aws_s3_bucket" "dl" {
   }
 }
 
-
-resource "aws_s3_bucket" "stream" {
-  bucket = "igti-ney-streaming-bucket"
-  acl    = "private"
-
-  tags = {
-    IES   = "IGTI",
-    CURSO = "EDC"
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
